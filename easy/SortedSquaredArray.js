9a2485ac62b6f1c258ba5d0af8d33119bd45fddd
AndrewSimons3 <simdrew07@gmail.com>
2022-02-01 17:37:24 -0600

    sortedSquaredArray

---
+++ easy/SortedSquaredArray.js
@@ -0,0 +1,20 @@
+
+console.log('hello')
+const arr = [1,2,3,4,5,6,7,8,9]
+
+
+// //Solution 1
+// function sortedSquaredArray(array) {
+// 	// Write your code here.
+// 	return array
+// 		.map((num) => num ** 2)
+// 		.sort((a, b) => {
+// 			return a - b;
+// 		});
+// }
+
+function sortedSquaredArray(array) { 
+  
+}
+
+sortedSquaredArray(arr);
