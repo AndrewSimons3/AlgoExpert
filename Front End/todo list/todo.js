3d584186cb0430687ccfd6b65e5a5416f0150a51
AndrewSimons3 <simdrew07@gmail.com>
2022-02-10 18:19:43 -0600

    todo list complete

--- Front End/todo list/todo.js
+++ Front End/todo list/todo.js
@@ -1,3 +1,35 @@
+const input = document.getElementById('todo-input');
+const addButton = document.getElementById('add-button');
+const todoList = document.getElementById('todo-list');
 
+input.addEventListener('input', typeTodo);
+addButton.addEventListener('click', addTodo);
 
+function addTodo() {
+  const li = createListItem(input.value);
+  todoList.appendChild(li);
+  addButton.disabled = true;
+  input.value = ''
+}
 
+function createListItem(name) {
+  const listItem = document.createElement('li');
+  const heading = document.createElement('h2');
+  heading.textContent = name;
+  const deleteButton = document.createElement('button');
+  deleteButton.textContent = 'X';
+  deleteButton.classList.add('delete-button');
+  deleteButton.addEventListener('click', deleteTodo);
+  listItem.appendChild(heading);
+  listItem.appendChild(deleteButton);
+
+  return listItem;
+}
+
+function typeTodo() {
+  addButton.disabled = input.value.length === 0;
+}
+
+function deleteTodo() {
+  this.parentNode.remove();
+}
