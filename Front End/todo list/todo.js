4132ecbd54c947f3364730f6d6977fd71b5c37bb
AndrewSimons3 <simdrew07@gmail.com>
2022-02-14 08:05:24 -0600

    practice

--- Front End/todo list/todo.js
+++ Front End/todo list/todo.js
@@ -2,34 +2,35 @@ const input = document.getElementById('todo-input');
 const addButton = document.getElementById('add-button');
 const todoList = document.getElementById('todo-list');
 
-input.addEventListener('input', typeTodo);
-addButton.addEventListener('click', addTodo);
+input.addEventListener('input', onTypeTodo);
+addButton.addEventListener('click', onClickAdd);
 
-function addTodo() {
-  const li = createListItem(input.value);
-  todoList.appendChild(li);
-  addButton.disabled = true;
-  input.value = ''
+function onClickAdd() {
+	const li = createListItem(input.value);
+	todoList.appendChild(li);
+	input.value = '';
+	addButton.disabled = true;
 }
 
-function createListItem(name) {
-  const listItem = document.createElement('li');
-  const heading = document.createElement('h2');
-  heading.textContent = name;
-  const deleteButton = document.createElement('button');
-  deleteButton.textContent = 'X';
-  deleteButton.classList.add('delete-button');
-  deleteButton.addEventListener('click', deleteTodo);
-  listItem.appendChild(heading);
-  listItem.appendChild(deleteButton);
-
-  return listItem;
+function onTypeTodo() {
+	addButton.disabled = input.value.length === 0;
 }
 
-function typeTodo() {
-  addButton.disabled = input.value.length === 0;
+function createListItem(value) {
+	const listItem = document.createElement('li');
+	const heading = document.createElement('h2');
+	heading.textContent = value;
+	const deleteButton = document.createElement('button');
+	deleteButton.textContent = 'X';
+	deleteButton.classList.add('delete-button');
+	deleteButton.addEventListener('click', onClickDelete);
+
+	listItem.appendChild(heading);
+	listItem.appendChild(deleteButton);
+
+	return listItem;
 }
 
-function deleteTodo() {
-  this.parentNode.remove();
+function onClickDelete() {
+	this.parentNode.remove();
 }
